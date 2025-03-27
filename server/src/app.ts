42ca59623377742b49f2c4586d6c3ef73861dd88
monkamherman <cesaristos5@gmail.com>
2025-03-27 01:46:20 +0100

    terminer up 5

--- server/src/app.ts
+++ server/src/app.ts
@@ -37,7 +37,14 @@ const corsOptions = {
 };
 
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
+app.options('*', (req, res) => res.sendStatus(200));
 
 // Middlewares critiques en premier
 app.use(cors(corsOptions));
